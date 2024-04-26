bcfd37c079fe1d268a119dce23156cbf21121030
maciejbilinski <maciej.bilinski.programming@gmail.com>
2024-04-26 13:54:13 +0200

    ASP.NET

---
+++ MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+
+        [DataType(dataType: DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 140, MinimumLength = 5)]
+        public string Text { get; set; } = string.Empty;
+
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
